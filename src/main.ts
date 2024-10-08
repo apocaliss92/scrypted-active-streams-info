31ca0fb04a43771727a1ac9b12602d956a0e8741
Gianluca Ruocco <gianluca.ruocco@xarvio.com>
2024-10-08 19:44:38 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,354 @@
-// https://developer.scrypted.app/#getting-started
-// package.json contains the metadata (name, interfaces) about this device
-// under the "scrypted" key.
-import { OnOff, ScryptedDeviceBase } from '@scrypted/sdk';
+import sdk, { ScryptedDeviceBase, ScryptedInterface, Setting, Settings } from "@scrypted/sdk";
+import { camerasKey, peopleKey, intervalIdKey, ipsKey, mqttHostKey, mqttUsernameKey, mqttPasswordKey, KnownPersonResult, StreamInfo } from "./types";
+import { connect, Client } from 'mqtt';
 
-console.log('Hello World. This will create a virtual OnOff device.');
-// OnOff is a simple binary switch. See "interfaces"  in package.json
-// to add support for more capabilities, like Brightness or Lock.
+const { systemManager } = sdk;
 
-class TypescriptLight extends ScryptedDeviceBase implements OnOff {
-    constructor(nativeId?: string) {
+export default class ActiveStreamsConfig extends ScryptedDeviceBase implements Settings {
+    mqttClient: Client;
+    lastSetMap: { [topic: string]: any } = {};
+    intervalId = new Date().getTime();
+    autodiscoveryPublished = false;
+    mqttPathmame: string;
+
+    constructor(nativeId: string) {
         super(nativeId);
-        this.on = this.on || false;
-    }
-    async turnOff() {
-        this.console.log('turnOff was called!');
-        this.on = false;
-    }
-    async turnOn() {
-        // set a breakpoint here.
-        this.console.log('turnOn was called!');
 
-        this.console.log("Let's pretend to perform a web request on an API that would turn on a light.");
-        const response = await fetch('http://jsonip.com');
-        const json = await response.json();
-        this.console.log(`my ip: ${json.ip}`);
+        this.start().catch(e => this.console.log(e));
+    }
 
-        this.on = true;
+    async getSettings(): Promise<Setting[]> {
+        const currentCameras = this.storage.getItem(camerasKey);
+        const currentPeopleRaw = this.storage.getItem(peopleKey);
+        const currentPeople = currentPeopleRaw ? currentPeopleRaw.split(',') : []
+
+        const settings: Setting[] = [
+            {
+                key: intervalIdKey,
+                type: 'string',
+                title: 'Interval ID',
+                readonly: true,
+                value: this.storage.getItem(intervalIdKey)
+            },
+            {
+                title: 'Host',
+                group: 'MQTT',
+                key: mqttHostKey,
+                description: 'Specify the mqtt address.',
+                placeholder: 'mqtt://192.168.1.100',
+                value: this.storage.getItem(mqttHostKey)
+            },
+            {
+                title: 'Username',
+                group: 'MQTT',
+                key: mqttUsernameKey,
+                description: 'Specify the mqtt username.',
+                value: this.storage.getItem(mqttUsernameKey)
+            },
+            {
+                title: 'Password',
+                group: 'MQTT',
+                key: mqttPasswordKey,
+                description: 'Specify the mqtt password.',
+                type: 'password',
+                value: this.storage.getItem(mqttPasswordKey)
+            },
+            {
+                group: 'Tracked entities',
+                key: camerasKey,
+                type: 'device',
+                title: 'Cameras',
+                multiple: true,
+                deviceFilter: `interfaces.includes("${ScryptedInterface.VideoCamera}")`,
+                value: currentCameras ? currentCameras.split(',') : [],
+            },
+            {
+                group: 'Tracked entities',
+                key: peopleKey,
+                type: 'string',
+                title: 'People',
+                multiple: true,
+                value: currentPeople,
+            }
+        ];
+
+        currentPeople.forEach(person => {
+            const personIpsKey = `${ipsKey}:${person}`;
+            const currentIps = this.storage.getItem(personIpsKey);
+            settings.push({
+                group: 'Tracked entities',
+                key: personIpsKey,
+                subgroup: person,
+                type: 'string',
+                title: `IPs`,
+                multiple: true,
+                value: currentIps ? currentIps.split(',') : [],
+            })
+        });
+
+        return settings;
+    }
+
+    async putSetting(key: string, value: string) {
+        this.storage.setItem(key, value.toString());
+        this.onDeviceEvent(ScryptedInterface.Settings, undefined);
+    }
+
+    mapToStreamInfo(setting: Setting) {
+        const fullIp = setting.subgroup.split(':');
+        fullIp.pop();
+        const ip = fullIp.join(':');
+        return { camera: setting.group, ip };
+    }
+
+    async processCamera(cameraId: string, settings: Setting[], activeStreamsConfigs: Setting[], isWhitelisted: boolean, knownPeople: string[]) {
+        const camera = systemManager.getDeviceById(cameraId);
+        const cameraName = camera.name;
+        const cameraStreamsInformation = settings.filter(setting => setting.group === camera.name);
+
+        const knownPeopleResult: KnownPersonResult[] = [];
+
+        const activeClients = Number(cameraStreamsInformation.find(setting => setting.title === 'Active Streams')?.value ?? 0);
+
+        for (const name of knownPeople) {
+            const ips = (activeStreamsConfigs.find(setting => setting.key === `activeStreamsIps:${name}`)?.value ?? []) as string[];
+            const personActiveStreams: StreamInfo[] = (cameraStreamsInformation
+                .filter(setting => ips.some(ip => setting.subgroup?.startsWith(ip)) && setting.key === 'type') ?? [])
+                .map(this.mapToStreamInfo);
+
+            knownPeopleResult.push({
+                cameraName,
+                cameraId,
+                person: name,
+                settings: personActiveStreams
+            })
+
+            if (isWhitelisted) {
+                const isPersonActive = !!personActiveStreams.length;
+                const isPersonActiveStreamTopic = `scrypted/activeStreams/${cameraId}/${name}`;
+                const personStreamsInfoTopic = `${isPersonActiveStreamTopic}/info`;
+                const lastIsPersonActiveStreamsSet = this.lastSetMap[isPersonActiveStreamTopic];
+                if (lastIsPersonActiveStreamsSet !== isPersonActive) {
+                    this.publish(isPersonActiveStreamTopic, isPersonActive);
+                    this.publish(personStreamsInfoTopic, { streams: personActiveStreams });
+                    this.lastSetMap[isPersonActiveStreamTopic] = isPersonActive;
+                }
+            }
+        }
+
+        if (isWhitelisted) {
+            const activeStreamsTopic = `scrypted/activeStreams/${cameraId}`;
+            const activeStreamsInfoTopic = `${activeStreamsTopic}/info`;
+            const lastActiveStreamsSet = this.lastSetMap[activeStreamsTopic];
+            if (lastActiveStreamsSet !== activeClients) {
+                this.publish(activeStreamsTopic, activeClients);
+                this.publish(activeStreamsInfoTopic, { streams: knownPeopleResult.flatMap(elem => elem.settings) });
+                this.lastSetMap[activeStreamsTopic] = activeClients;
+            }
+        }
+
+        return { activeClients, knownPeopleResult, cameraName };
+    }
+
+    async start() {
+        this.getMqttClient();
+        await this.putSetting(intervalIdKey, JSON.stringify(this.intervalId));
+
+        const currentInterval = setInterval(async () => {
+            try {
+                if (!this.mqttClient || !this.mqttClient.connected) {
+                    return;
+                }
+
+                const activeStreamsConfigs = await this.getSettings();
+                //this.console.log(`Active streams configs: ${JSON.stringify(activeStreamsConfigs, undefined, 2)}`);
+                const currentIntervalId = Number(activeStreamsConfigs.find(setting => setting.key === intervalIdKey)?.value);
+                const whitelistedCameraIds = (activeStreamsConfigs.find(setting => setting.key === camerasKey)?.value as string[]);
+                const knownPeople = (activeStreamsConfigs.find(setting => setting.key === peopleKey)?.value ?? []) as string[];
+
+                const cameraIds: string[] = [];
+
+                Object.entries(systemManager.getSystemState()).forEach(([deviceId, device]) => {
+                    if (device.interfaces.value.includes(ScryptedInterface.VideoCamera)) {
+                        cameraIds.push(deviceId);
+                    }
+                })
+
+                const cameraData: { id: string, name: string, activeStreams: number }[] = [];
+                const peopleData: { [person: string]: StreamInfo[] } = {};
+
+                if (currentIntervalId && currentIntervalId > this.intervalId || !cameraIds || cameraIds.length === 0) {
+                    this.console.log('Clearing interval because newer script is running');
+                    clearInterval(currentInterval);
+                } else {
+                    let totalActiveStreams = 0;
+                    const totalKnownPeopleResult: KnownPersonResult[] = [];
+
+                    const adaptivePlugin = systemManager.getDeviceByName('Adaptive Streaming') as unknown as (Settings);
+                    const settings = await adaptivePlugin.getSettings();
+
+                    this.console.log(`All settings: ${JSON.stringify(settings, undefined, 2)}`);
+
+                    for (const cameraId of cameraIds) {
+                        const isWhitelisted = whitelistedCameraIds.includes(cameraId);
+                        const { activeClients, knownPeopleResult, cameraName } = await this.processCamera(cameraId, settings, activeStreamsConfigs, isWhitelisted, knownPeople);
+                        totalActiveStreams += activeClients;
+                        totalKnownPeopleResult.push(...knownPeopleResult);
+
+                        knownPeopleResult.forEach(elem => {
+                            const { person, settings } = elem;
+                            if (!peopleData[person]) {
+                                peopleData[person] = [];
+                            }
+
+                            peopleData[person].push(...settings);
+                        });
+
+                        cameraData.push({ name: cameraName, id: cameraId, activeStreams: activeClients });
+                    }
+
+                    for (const person of knownPeople) {
+                        const personStreams = peopleData[person] ?? [];
+                        const activeClients = personStreams.length;
+
+                        const activeStreamsTopic = `scrypted/activeStreams/${person}`;
+                        const activeStreamsInfoTopic = `${activeStreamsTopic}/info`;
+                        const lastActiveStreamsSet = this.lastSetMap[activeStreamsTopic];
+                        if (lastActiveStreamsSet !== activeClients) {
+                            this.publish(activeStreamsTopic, activeClients);
+                            this.publish(activeStreamsInfoTopic, { streams: personStreams });
+                            this.lastSetMap[activeStreamsTopic] = activeClients;
+                        }
+                    }
+
+                    const totalActiveStreamsTopic = `scrypted/activeStreams`;
+                    const totalActiveStreamsInfoTopic = `${totalActiveStreamsTopic}/info`;
+                    const lastTotalActiveStreamsSet = this.lastSetMap[totalActiveStreamsTopic];
+                    if (lastTotalActiveStreamsSet !== totalActiveStreams) {
+                        this.publish(totalActiveStreamsTopic, totalActiveStreams);
+                        this.publish(totalActiveStreamsInfoTopic, 
+                            { 
+                                streams: settings
+                                .filter(setting => setting.key === 'type')
+                                .map(this.mapToStreamInfo) 
+                            });
+                        this.lastSetMap[totalActiveStreamsTopic] = totalActiveStreams;
+                    }
+
+                    if (!this.autodiscoveryPublished) {
+                        for (const data of cameraData) {
+                            const { id: cameraId, name: cameraName } = data;
+                            if (whitelistedCameraIds.includes(cameraId)) {
+                                for (const name of knownPeople) {
+                                    const configTopic = `homeassistant/binary_sensor/scrypted-active-stream-${cameraId}-${name}/ActiveStream/config`;
+                                    const stateTopic = `scrypted/activeStreams/${cameraId}/${name}`;
+                                    const config = {
+                                        state_topic: stateTopic,
+                                        json_attributes_topic: `${stateTopic}/info`,
+                                        json_attributes_template: '{{ value_json | tojson }}',
+                                        payload_on: 'true',
+                                        payload_off: 'false',
+                                        dev: {
+                                            ids: `scrypted-activeStream-${cameraId}-${name}`,
+                                            name: `Active stream for ${cameraName} ${name}`
+                                        },
+                                        unique_id: `scrypted-active-stream-${cameraId}-${name}`,
+                                        name: `${cameraName} ${name} active`,
+                                    };
+                                    this.publish(configTopic, JSON.stringify(config));
+                                }
+
+                                const configTopic = `homeassistant/sensor/scrypted-active-streams-${cameraId}/ActiveStreams/config`;
+                                const stateTopic = `scrypted/activeStreams/${cameraId}`;
+                                const config = {
+                                    state_topic: stateTopic,
+                                    json_attributes_topic: `${stateTopic}/info`,
+                                    json_attributes_template: '{{ value_json | tojson }}',
+                                    dev: {
+                                        ids: `scrypted-activeStreams-${cameraId}`,
+                                        name: `Active streams for ${cameraName}`
+                                    },
+                                    unique_id: `scrypted-active-streams-${cameraId}`,
+                                    name: `${cameraName} active streams`,
+                                };
+                                this.publish(configTopic, JSON.stringify(config));
+                            }
+                        }
+
+                        for (const name of knownPeople) {
+                            const configTopic = `homeassistant/sensor/scrypted-active-stream-${name}/ActiveStream/config`;
+                            const stateTopic = `scrypted/activeStreams/${name}`;
+                            const config = {
+                                state_topic: stateTopic,
+                                json_attributes_topic: `${stateTopic}/info`,
+                                json_attributes_template: '{{ value_json | tojson }}',
+                                dev: {
+                                    ids: `scrypted-activeStreams-${name}`,
+                                    name: `Active streams for ${name}`
+                                },
+                                unique_id: `scrypted-active-streams-${name}`,
+                                name: `${name} active streams`,
+                            };
+                            this.publish(configTopic, JSON.stringify(config));
+                        }
+
+                        const configTopic = `homeassistant/sensor/scrypted-active-streams/ActiveStreams/config`;
+                        const stateTopic = `scrypted/activeStreams`;
+                        const config = {
+                            state_topic: stateTopic,
+                            json_attributes_topic: `${stateTopic}/info`,
+                            json_attributes_template: '{{ value_json | tojson }}',
+                            dev: {
+                                ids: `scrypted-activeStreams`,
+                                name: `Active streams`
+                            },
+                            unique_id: `scrypted-active-streams`,
+                            name: `All active streams`,
+                        };
+                        this.publish(configTopic, JSON.stringify(config));
+
+                        this.autodiscoveryPublished = true;
+                    }
+                }
+            } catch (e) {
+                clearInterval(currentInterval);
+                this.console.log(e);
+            }
+        }, 10000);
+    }
+
+    async getMqttClient() {
+        if (!this.mqttClient) {
+            const url = this.storage.getItem(mqttHostKey);
+            const urlWithoutPath = new URL(url);
+            urlWithoutPath.pathname = '';
+
+            this.mqttPathmame = urlWithoutPath.toString();
+            if(!this.mqttPathmame.endsWith('/')) {
+                this.mqttPathmame = `${this.mqttPathmame}/`;
+            }
+            this.mqttClient = connect(this.mqttPathmame, {
+                rejectUnauthorized: false,
+                username: this.storage.getItem(mqttUsernameKey) || undefined,
+                password: this.storage.getItem(mqttPasswordKey) || undefined,
+            });
+            this.mqttClient.setMaxListeners(Infinity);
+
+            this.mqttClient.on('connect', packet => {
+                this.console.log('connected to mqtt', packet);
+            })
+        }
+
+        return this.mqttClient;
+    }
+
+    async publish(topic: string, value: any) {
+        if (typeof value === 'object')
+            value = JSON.stringify(value);
+        if (value.constructor.name !== Buffer.name)
+            value = value.toString();
+        
+        this.console.log(`Publishing ${value} to ${topic}`);
+        (await this.getMqttClient()).publish(topic, value, { retain: true });
     }
 }
-
-export default TypescriptLight;
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export const intervalIdKey = 'activeStreamsIntervalId';
+export const camerasKey = 'activeStreamsCameras';
+export const peopleKey = 'activeStreamsPeople';
+export const ipsKey = 'activeStreamsIps';
+export const mqttHostKey = 'activeStreamsMqttHost';
+export const mqttUsernameKey = 'activeStreamsMqttUsername';
+export const mqttPasswordKey = 'activeStreamsMqttPassword';
+
+export interface StreamInfo {
+    ip: string;
+    camera: string;
+}
+
+export interface KnownPersonResult {
+    cameraName: string,
+    cameraId: string, 
+    person: string, 
+    settings: StreamInfo[]
+  }
+
